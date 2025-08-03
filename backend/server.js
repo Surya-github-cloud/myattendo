cf8e743cada9c0a5bc34894de61d42f6bf80c817
Surya-github-cloud <suryavamsepdp@gmail.com>
2025-08-03 08:48:10 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -60,4 +60,4 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
